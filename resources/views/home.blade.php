77bff16c2171b61e742c379fb88c765f35b7e15f
estebanm <esteban.morgade@oasisent.com.ar>
2025-07-02 18:45:37 +0200

    Update home & nav styles

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layout')
 
-@section('title', 'Home')
+@section('title', __(('Home')))
 
 @section('content')
 
@@ -8,19 +8,19 @@
       <div class="row">
         
         <div class="col-12 col-lg-6">
-            <h1 class="display-4 text-primary">Desarrollo web</h1>
+            <h1 class="display-4 text-primary">{{__(('Home'))}}</h1>
             <p class="lead text-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
               Eum soluta, illum fuga deserunt nobis voluptatem aut cum laudantium quidem. 
               Earum, illum omnis quasi alias dolores rerum aut dolorem tempora amet?
             </p>
             <div class="d-flex flex-column gap-2">
-                <a class="btn btn-lg btn-primary" href="{{ route('contact') }}">{{__('Contactame')}}</a>
-                <a class="btn btn-lg btn-outline-primary" href="{{ route('projects.index') }}">{{__('Projects')}}</a>
+                <a class="btn btn-lg btn-primary" href="{{ route('contact') }}">{{__(('Contact'))}}</a>
+                <a class="btn btn-lg btn-outline-primary" href="{{ route('projects.index') }}">{{__(('Projects'))}}</a>
             </div>
         </div>
 
         <div class="col-12 col-lg-6">
-            <img class="img-fluid" src="/img/home.svg" alt="Desarrollo web">
+            <img class="img-fluid" src="/img/home.svg" alt="">
         </div>
 
       </div>
